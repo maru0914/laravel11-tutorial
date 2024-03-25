c4747adb93a841a54e34b165bfa2387f9b3a51a8
emmaru <littleoduozu@gmail.com>
2024-03-25 21:44:52 +0900

    EloquentModelの作成

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'ディレクター',
-                'salary'=> '750万円'
-            ],
-            [
-                'id' => 2,
-                'title' => 'プログラマー',
-                'salary' => '150万円'
-            ],
-            [
-                'id' => 3,
-                'title' => '教師',
-                'salary' => '600万円'
-            ],
-        ];
-    }
+class Job extends Model
+{
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
