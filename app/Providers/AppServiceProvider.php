cc76a515b84b075cc6b8c9ec6e360e656657e144
emmaru <littleoduozu@gmail.com>
2024-04-17 21:56:48 +0900

    ログイン・ログアウト機能の実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
-        Model::unguard();
-
-//        Paginator::useBootstrapFive();
     }
 }
